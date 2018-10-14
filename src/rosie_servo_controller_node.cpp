060532ef5b5a71cccb6f8dd0bf26c72aaf716792
TNeverWood <tobiasniewalda@web.de>
2018-10-14 16:00:35 +0200

    not working on NUC, needs debugging

--- src/rosie_servo_controller_node.cpp
+++ src/rosie_servo_controller_node.cpp
@@ -5,6 +5,41 @@
 
 int saveright = 0;
 int saveleft = 0;
+int saveright2 = 0;
+int saveleft2 = 0;
+
+void GateCallback(const rosie_servo_controller::GateAngles& msg){
+
+    int leftgate = msg.leftgate;
+    int rightgate = msg.rightgate;
+
+    if(rightgate != saveright)
+    {
+	arduino_servo_control::SetServoAngles srv;
+
+	if(saveright != saveright2)
+  	{
+	  arduino_servo_control::SetServoAngles srv;
+	  srv.request.angle_servo_0 = saveright;
+	  srv.request.angle_servo_1 = saveleft;
+
+		if (ros::service::call("SetServoAngles", srv))
+		{
+		  ROS_INFO("Gates actuated to: LEFT: %d     RIGHT: %d", saveleft,saveright);
+  		  ROS_INFO("Previous values  : LEFT: %d     RIGHT: %d", saveleft2,saveright2);
+		}
+		else
+		{
+		  ROS_ERROR("Failed to call service SetServoAngles");
+		}
+	  saveright2 = saveright;
+	  saveleft2 = saveleft;
+        } 
+	saveleft = leftgate;
+        saveright = rightgate;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
@@ -12,36 +47,11 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::ServiceClient gateclient = n.serviceClient<arduino_servo_control::SetServoAngles>("SetServoAngles");
+  //ros::ServiceClient client = n.serviceClient<arduino_servo_control::SetServoAngles>("SetServoAngles");
 
-  ros::Subscriber sub = n.subscribe("Gates", 1000, GateCallback);
+  ros::Subscriber sub = n.subscribe("MoveGates", 1000, GateCallback);
 
   ros::spin();
 
   return 0;
-}
-
-void Gatecallback(const rosie_servo_contoller::GateAngles& msg){
-
-    int leftgate = msg->angle_left;
-    int rightgate = msg->angle_right;
-
-    if(rightgate != saveright)
-    {
-        arduino_servo_control::SetServoAngles srv;
-        srv.request.angle_servo_0 = x;
-        srv.request.angle_servo_1 = y;
-
-        if (client.call(srv))
-        {
-          ROS_INFO("Gates actuated to: LEFT: %d     RIGHT: %d", saveleft,saveright);
-        }
-        else
-        {
-          ROS_ERROR("Failed to call service SetServoAngles");
-          return 1;
-        }
-        saveleft = leftgate;
-        saveright = rightgate;
-    }
 }
